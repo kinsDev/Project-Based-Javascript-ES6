46788cc4b83ec19000913639eb49bb75c97eda88
Eng-Kinsley <kinsleygitonga@gmail.com>
2023-04-03 05:04:13 +0300

    updated code

---
+++ Switch_Statements_#5/statements.js
@@ -0,0 +1,18 @@
+//switch statements
+/**
+ * switch(expression) {
+ * case value1:
+    * statement to be executed
+    * match value1
+    * break;
+case value2:
+    statement to be executed
+    match value2
+    break;
+case value3:
+    ...
+    break;
+default:
+    break;
+ * }
+ */
